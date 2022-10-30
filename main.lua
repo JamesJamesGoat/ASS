c8fc187cc2e1fb1275704b221b9b3cfeed1bc430
JamesJamesGoat <115616141+JamesJamesGoat@users.noreply.github.com>
2022-10-30 15:29:00 +1000

    Update main.lua

--- main.lua
+++ main.lua
@@ -271,7 +271,7 @@ local buffTable = {
     ["Glue"] = {b = false, DecalID = "2504978518"},
     ["Glitter"] = {b = false, DecalID = "2542899798"},
     ["Tropical Drink"] = {b = false, DecalID = "3835877932"},
-    ["Jelly Bean"] = {b = false, DecalID = "3080740120"};
+    ["Jelly Bean"] = {b = false, DecalID = "3080740120"},
 }
 local AccessoryTypes = require(game:GetService("ReplicatedStorage").Accessories).GetTypes()
 local MasksTable = {}
