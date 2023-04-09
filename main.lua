1f51a3733db806d0cad739b3a8c0df14653a4995
JamesJamesGoat <115616141+JamesJamesGoat@users.noreply.github.com>
2023-04-09 20:00:27 +1000

    Update main.lua

--- main.lua
+++ main.lua
@@ -470,8 +470,8 @@ getgenv().macrov2 = {
         convertat = 100,
         farmspeed = 60,
         prefer = "Tokens",
-        walkspeed = 70,
-        jumppower = 70,
+        walkspeed = 56,
+        jumppower = 80,
         npcprefer = "All Quests",
         farmtype = "Walk",
         monstertimer = 15,
@@ -2591,7 +2591,7 @@ guiElements["vars"]["convertballoonpercent"] = balloonPercentSlider
 guiElements["toggles"]["enabletokenblacklisting"] = farmsettings:CreateToggle("Enable Token Blacklisting", nil, function(State)
     macrov2.toggles.enabletokenblacklisting = State
 end)
-guiElements["vars"]["walkspeed"] = farmsettings:CreateSlider("Walk Speed", 0, 120, 70, false, function(Value)
+guiElements["vars"]["walkspeed"] = farmsettings:CreateSlider("Walk Speed", 0, 120, 56, false, function(Value)
     macrov2.vars.walkspeed = Value
 end)
 guiElements["vars"]["jumppower"] = farmsettings:CreateSlider("Jump Power", 0, 200, 80, false, function(Value)
@@ -3199,18 +3199,7 @@ task.spawn(function()
                             task.wait(0.5)
                             playeractivescommand:FireServer({["Name"] = "Sprinkler Builder"})
                         end
-                    else
-                        if not game.Workspace.MonsterSpawners.CoconutCrab.Attachment.TimerGui.TimerLabel.Visible and not temptable.started.vicious and not temptable.started.monsters and not temptable.started.windy and findField(fieldposition).Name == "Coconut Field" then
-                            maskequip("Demon Mask")
-                            temptable.started.crab = true
-                            while not game.Workspace.MonsterSpawners.CoconutCrab.Attachment.TimerGui.TimerLabel.Visible and not temptable.started.vicious and not temptable.started.monsters and not temptable.started.windy and findField(fieldposition).Name == "Coconut Field" do
-                                task.wait()
-                                if api.humanoidrootpart() then
-                                    api.tween(3, CFrame.new(-307.52117919922, 110.11863250732, 467.86791992188))
-                                end
-                            end
-                        end
-                        temptable.started.crab = false
+
 			if macrov2.toggles.killmondo then
                             while macrov2.toggles.killmondo and game.Workspace.Monsters:FindFirstChild("Mondo Chick (Lvl 8)") and not temptable.started.vicious and not temptable.started.monsters do
                                 temptable.started.mondo = true
